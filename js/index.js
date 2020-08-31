17ec19c14e4c81bb6f70e62a83cc1940b3412272
Ehsan Gazar <me@ehsangazar.com>
2020-08-31 14:43:11 +1000

    second video basic calculations

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 let x = 0
+let y = 0
+
+let operator = null
 
 const onLoad = () => {
     console.log('onLoad')
@@ -6,10 +9,60 @@ const onLoad = () => {
 }
 
 const pressNumber = (number) => {
-    x = x*10 + number
-    handleUpdateScreen(x)
+    if (operator){
+        y = y * 10 + number
+        handleUpdateScreen(y)
+    }else {
+        x = x*10 + number
+        handleUpdateScreen(x)
+    }
 }
 
 const handleUpdateScreen = (newNumber) => {
     document.getElementById('screen').innerHTML = newNumber
+}
+
+const pressOperator = (newOperator) => {
+    operator = newOperator
+    document.getElementById(newOperator).classList.add('active')
+}
+
+const handleClearOperatorButtons = () => {
+    document.querySelectorAll('.operator').forEach((node) => {
+        node.classList.remove('active')
+    })
+}
+
+const handleClear = () => {
+    x = 0;
+    operator = null
+    handleUpdateScreen(x)
+    handleClearOperatorButtons();
+}
+
+const handleEval = () => {
+    let result = 0;
+    switch (operator) {
+        case 'plus':
+            result = x + y
+            break;
+        case 'minus':
+            result = x - y
+            break;
+        case 'multiple':
+            result = x * y
+            break;
+        case 'divide':
+            result = x / y
+            break;
+    
+        default:
+            break;
+    }
+
+    x = result;
+    y = 0;
+    handleUpdateScreen(x)
+    handleClearOperatorButtons()
+    operator = null
 }
